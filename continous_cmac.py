f851bd05d22abd69029b7b4d4e884d81e8539cae
Bernard3073 <bert41003@gmail.com>
2022-02-16 11:55:29 -0500

    writing it in a different way

--- continous_cmac.py
+++ continous_cmac.py
@@ -1,85 +1,140 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import time
+from tqdm import tqdm
 '''
 35 weights
 sample function at 100
 training: 70, testing: 30
 '''
 def main():
-    # X = np.linspace(0, 2 * np.pi, 100)
-    X = np.linspace(0, 10, 100)
+    X = np.linspace(0, 2 * np.pi, 100)
+    # X = np.linspace(0, 10, 100)
     Y = np.sin(X)
     training_data_idx = np.random.choice(len(X), size=70, replace=False)
+    training_data_idx.sort()
     training_data = [[X[i], Y[i]] for i in training_data_idx]
     testing_data_idx = []
     for i in range(100):
         if i not in training_data_idx:
             testing_data_idx.append(i)
-    testing_data = [[X[i], Y[i]] for i in testing_data_idx]
-    weight = 35
-    weight_arr = np.zeros(weight).tolist()
-    epoch_count = 1
-    num_epoch = 100
-    window_size = 10
-    overlap = 35
-    learning_rate = 0.1
-    data_over_epoch = []
-    overlap_data = []
-    for o in range(1,overlap):
-        while epoch_count <= num_epoch:
+    testing_data = [X[i] for i in testing_data_idx]
+    weight = 0.0
+    w_zero = []
+    w_save =  w_zero
+    error = mean_sq_array = total_time= gen_list = w_rand = weight_save = []
+    start = time.time()     
+    gen = 3
+    mean_square_error = 1
+    for g in tqdm(range(1, 37,2)):
+        # generate the weight array with size 35 
+        weight_arr = np.random.rand(35)
+        padding = int((g - 1) / 2)
+        # Creating padding on both ends
+        weight_arr = np.append(np.zeros((padding)), weight_arr)
+        weight_arr = np.append(weight_arr, np.zeros(padding))
 
-            pred_list = []
-            error_list = []
-            for i in range(70):
-                cur = training_data[i][0]
-                desire = training_data[i][1]
+
+        while mean_square_error > 0.01: 
+            # Training 
+            for j in range(0, 70):
+                q = j / 2
+
                 
-                associate_func = weight * (cur/window_size)
-                lower = 0 if associate_func - (o/2) < 0 else associate_func - (o/2) 
-                upper = weight-1 if associate_func + (o/2) > weight-1 else associate_func + (o/2) 
-                # print(lower, upper)
-                pred = 0
-                for j in range(int(lower)+1, int(upper)):
-                    pred += weight_arr[j] * cur
-                pred += weight_arr[int(lower)] * 0.8
-                pred += weight_arr[int(upper)] * 0.2
-                pred_list.append(pred)
 
-                error = desire - pred
-                error_list.append(error)
-                # error correction
-                corrected_val = error / overlap
-                # update weights
-                for i in range(int(lower)+1, int(upper)):
-                    weight_arr[i] += learning_rate * corrected_val
-                weight_arr[int(lower)] += learning_rate * corrected_val * 0.8
-                weight_arr[int(upper)] += learning_rate * corrected_val * 0.2
-            
-            data_over_epoch.append([pred, error, weight_arr])
-            epoch_count += 1
-        # testing
-        error_list_testing = []
-        pred_list_testing = []
-        for i in range(30):
-            cur = testing_data[i][0]
-            desire = testing_data[i][1]
+                w_new_value =   weight / g
+                y_train = training_data[j][1]
+
+                e = y_train - w_new_value
+                error = np.append(error, e)
+                # Error Correction
+                corrected_val = e / g  
+
                 
-            associate_func = weight * (cur/window_size)
-            lower = 0 if associate_func - (o/2) < 0 else associate_func - (o/2) 
-            upper = weight-1 if associate_func + (o/2) > weight-1 else associate_func + (o/2) 
-            # predict output
-            pred = 0
-            for j in range(int(lower)+1, int(upper)):
-                pred += weight_arr[j] * cur
-            pred += weight_arr[int(lower)] * 0.8
-            pred += weight_arr[int(upper)] * 0.2
-            pred_list_testing.append(pred)
-            # calculate error
-            error = desire - pred
-            error_list_testing.append(error)
+                
+                weight = 0
 
-        plt.plot(X, Y)
-        plt.plot([testing_data[i][0] for i in range(30)], pred_list_testing)
+
+def main():
+    X = np.linspace(0, 2 * np.pi, 100)
+    # X = np.linspace(0, 10, 100)
+    Y = np.sin(X)
+    training_data_idx = np.random.choice(len(X), size=70, replace=False)
+    training_data_idx.sort()
+    training_data = [[X[i], Y[i]] for i in training_data_idx]
+    testing_data_idx = []
+    for i in range(100):
+        if i not in training_data_idx:
+            testing_data_idx.append(i)
+    testing_data = [X[i] for i in testing_data_idx]
+    weight = 0
+    error_arr = mean_sq_arr = total_time= gen_list = weight_arr_save = []
+    start = time.time()   
+    gen = 5
+    mean_square_error = 1
+    for g in tqdm(range(1, 37, 2)):
+        # generate the weight array with size 35 
+        weight_arr = np.random.rand(35)
+        padding = int((g - 1) / 2)
+        # Creating padding on both ends
+        weight_arr = np.append(np.zeros((padding)), weight_arr)
+        weight_arr = np.append(weight_arr, np.zeros(padding))
+
+        while mean_square_error > 0.05: 
+            # Training 
+            for j in range(0, 70):
+                q = int(j / 2)
+
+                # calculate the sum of weight
+                for k in range(1, g-1):
+                    weight += weight_arr[k + q]
+                weight += weight_arr[q] * 0.7
+                weight += weight_arr[g-1 + q] * 0.3
+                pred_output = weight / g
+                y_train = training_data[j][1]
+
+                error = y_train - pred_output
+                error_arr = np.append(error_arr, error)
+                # Error Correction
+                corrected_val = error / g  
+
+                for k in range(1, g-1):
+                    weight_arr[k + q] += corrected_val
+                weight_arr[q] += corrected_val * 0.7
+                weight_arr[g - 1 + q] += corrected_val * 0.3
+
+                weight = 0
+
+            mean_square_error = np.mean(error**2)
+
+        if g == gen:       
+            weight_arr_save = weight_arr
+
+        gen_list = np.append(gen_list, g)
+        end = time.time()
+        total_time = np.append(total_time, (end - start))
+        mean_sq_arr = np.append(mean_sq_arr, mean_square_error)
+        # reset mean_square_error 
+        mean_square_error = 1
+
+    gen_half = int(gen/2)
+    # save weight_arr in every half of the gen size steps
+    w_new = weight_arr_save[1::gen_half]
+    w_new_array = []
+    # Testing 
+    for i in range(0, 30):
+        q = int(i / 2)
+        w_avg = np.sum(w_new[q - gen_half:q + gen_half + 1]) / gen
+        w_new_array = np.append(w_new_array, w_avg)
+
+    # plotting results
+    plt.figure(1)
+    plt.plot(X, Y, '-k', label="Input", linewidth=2)
+    plt.plot(testing_data, w_new_array, '-r', label="Test")
+    plt.xlabel('x')
+    plt.ylabel('sin(x)')
+    plt.legend()
     plt.show()
+
 if __name__ == '__main__':
     main()
--- discrete_cmac.py
+++ discrete_cmac.py
@@ -1,78 +1,88 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import time
+from tqdm import tqdm
 '''
-35 weights
+35 weight_arrs
 sample function at 100
 training: 70, testing: 30
 '''
 def main():
-    # X = np.linspace(0, 2 * np.pi, 100)
-    X = np.linspace(0, 10, 100)
+    X = np.linspace(0, 2 * np.pi, 100)
+    # X = np.linspace(0, 10, 100)
     Y = np.sin(X)
     training_data_idx = np.random.choice(len(X), size=70, replace=False)
+    training_data_idx.sort()
     training_data = [[X[i], Y[i]] for i in training_data_idx]
     testing_data_idx = []
     for i in range(100):
         if i not in training_data_idx:
             testing_data_idx.append(i)
-    testing_data = [[X[i], Y[i]] for i in testing_data_idx]
-    weight = 35
-    weight_arr = np.zeros(weight).tolist()
-    epoch_count = 1
-    num_epoch = 100
-    window_size = 5
-    overlap = 35
-    learning_rate = 0.1
-    data_over_epoch = []
-    for o in range(1,overlap):
-        while epoch_count <= num_epoch:
+    testing_data = [X[i] for i in testing_data_idx]
+    weight = 0
+    error_arr = mean_sq_arr = total_time= gen_list = weight_arr_save = []
+    start = time.time()   
+    gen = 5
+    mean_square_error = 1
+    for g in tqdm(range(1, 37, 2)):
+        # generate the weight array with size 35 
+        weight_arr = np.random.rand(35)
+        padding = int((g - 1) / 2)
+        # Creating padding on both ends
+        weight_arr = np.append(np.zeros((padding)), weight_arr)
+        weight_arr = np.append(weight_arr, np.zeros(padding))
 
-            pred_list = []
-            error_list = []
-            for i in range(70):
-                cur = training_data[i][0]
-                desire = training_data[i][1]
-                
-                associate_func = weight * (cur/window_size)
-                lower = 0 if associate_func - (o/2) < 0 else associate_func - (o/2) 
-                upper = weight-1 if associate_func + (o/2) > weight-1 else associate_func + (o/2) 
-                # print(lower, upper)
-                pred = 0
-                for j in range(int(lower), int(upper)):
-                    pred += weight_arr[j] * cur
-                pred_list.append(pred)
+        while mean_square_error > 0.05: 
+            # Training 
+            for j in range(0, 70):
+                q = int(j / 2)
 
-                error = desire - pred
-                error_list.append(error)
-                # error correction
-                corrected_val = error / overlap
-                # update weights
-                for i in range(int(lower), int(upper)):
-                    weight_arr[i] += learning_rate * corrected_val
-            
-            data_over_epoch.append([pred, error, weight_arr])
-            epoch_count += 1
-        # testing
-        error_list_testing = []
-        pred_list_testing = []
-        for i in range(30):
-            cur = testing_data[i][0]
-            desire = testing_data[i][1]
-                
-            associate_func = weight * (cur/window_size)
-            lower = 0 if associate_func - (o/2) < 0 else associate_func - (o/2) 
-            upper = weight-1 if associate_func + (o/2) > weight-1 else associate_func + (o/2) 
+                # calculate the sum of weight
+                for k in range(g):
+                    weight += weight_arr[k + q]
 
-            pred = 0
-            for j in range(int(lower), int(upper)):
-                pred += weight_arr[j] * cur
-            pred_list_testing.append(pred)
+                pred_output = weight / g
+                y_train = training_data[j][1]
 
-            error = desire - pred
-            error_list_testing.append(error)
+                error = y_train - pred_output
+                error_arr = np.append(error_arr, error)
+                # Error Correction
+                corrected_val = error / g  
 
-        plt.plot(X, Y)
-        plt.plot([testing_data[i][0] for i in range(30)], pred_list_testing)
+                for k in range(g):
+                    weight_arr[k + q] += corrected_val
+
+                weight = 0
+
+            mean_square_error = np.mean(error**2)
+
+        if g == gen:       
+            weight_arr_save = weight_arr
+
+        gen_list = np.append(gen_list, g)
+        end = time.time()
+        total_time = np.append(total_time, (end - start))
+        mean_sq_arr = np.append(mean_sq_arr, mean_square_error)
+        # reset mean_square_error 
+        mean_square_error = 1
+
+    gen_half = int(gen/2)
+    # save weight_arr in every half of the gen size steps
+    w_new = weight_arr_save[1::gen_half]
+    w_new_array = []
+    # Testing 
+    for i in range(0, 30):
+        q = int(i / 2)
+        w_avg = np.sum(w_new[q - gen_half:q + gen_half + 1]) / gen
+        w_new_array = np.append(w_new_array, w_avg)
+
+    # plotting results
+    plt.figure(1)
+    plt.plot(X, Y, '-k', label="Input", linewidth=2)
+    plt.plot(testing_data, w_new_array, '-r', label="Test")
+    plt.xlabel('x')
+    plt.ylabel('sin(x)')
+    plt.legend()
     plt.show()
 if __name__ == '__main__':
     main()
